005238639f6d83635fb6c21cec2b86f2561e56fa
Matt Johnson <mj1856@hotmail.com>
2013-06-21 13:39:35 -0700

    Update to Syndll2 0.6.0

--- SynUtil/Properties/AssemblyInfo.cs
+++ SynUtil/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("SynUtil")]
 [assembly: AssemblyCopyright("Copyright © 2013, Synel Industries Ltd.")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.7.1")]
-[assembly: AssemblyFileVersion("1.7.1.16")]
+[assembly: AssemblyVersion("1.7.2")]
+[assembly: AssemblyFileVersion("1.7.2.17")]
